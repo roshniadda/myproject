f5b4083ceb0528a111ea59f6aa27c1f770b5c6f3
zoeybird <swati19093@gmail.com>
2016-02-07 16:50:38 +0530

    travis file added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.order_by('published_date')
@@ -10,3 +12,17 @@ def post_list(request):
 def post_detail(request,pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'post_detail.html', {'post': post})
+
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_list')
+	else:
+		form = PostForm()
+	return render(request, 'post_edit.html', {'form': form})
